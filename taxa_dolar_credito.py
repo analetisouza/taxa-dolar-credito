07838b5d04b0fedf25e54af608a889c09152140a
Ana Leticia Souza <analesouza@oi.com.br>
2020-10-06 22:16:11 -0300

    version with working df, history to be fixed

--- taxa_dolar_credito.py
+++ taxa_dolar_credito.py
@@ -1,21 +1,20 @@
 import json
 import requests
-import pandas
+import pandas as pd
 
-source_bradesco = requests.get("https://olinda.bcb.gov.br/olinda/servico/DASFN/versao/v1/odata/Recursos?$filter=CnpjInstituicao%20eq%20'60746948000112'&$format=json&$select=Api,CnpjInstituicao,NomeInstituicao,Recurso,URLDados")
-source_caixa = requests.get("https://olinda.bcb.gov.br/olinda/servico/DASFN/versao/v1/odata/Recursos?$filter=CnpjInstituicao%20eq%20'00360305000104'&$format=json&$select=Api,CnpjInstituicao,NomeInstituicao,Recurso,URLDados")
-source_itau = requests.get("https://olinda.bcb.gov.br/olinda/servico/DASFN/versao/v1/odata/Recursos?$filter=CnpjInstituicao%20eq%20'60701190000104'&$format=json&$select=Api,CnpjInstituicao,NomeInstituicao,Recurso,URLDados")
-source_nubank = requests.get("https://olinda.bcb.gov.br/olinda/servico/DASFN/versao/v1/odata/Recursos?$filter=CnpjInstituicao%20eq%20'18236120000158'&$format=json&$select=Api,CnpjInstituicao,NomeInstituicao,Recurso,URLDados")
 
-api_bradesco = json.loads(source_bradesco.text)
-api_caixa = json.loads(source_caixa.text)
-api_itau = json.loads(source_itau.text)
-api_nubank = json.loads(source_nubank.text)
+#source_caixa = requests.get("https://api.caixa.gov.br:8443/dadosabertos/taxasCartoes/1.2.0/itens")
+source_bradesco = "https://proxy.api.prebanco.com.br/bradesco/dadosabertos/taxasCartoes/itens"
+source_itau = requests.get("https://api.itau.com.br/dadosabertos/taxasCartoes/taxas/itens")
+source_nubank = requests.get("https://dadosabertos.nubank.com.br/taxasCartoes/itens")
 
-print(api_caixa)
+#data_caixa = json.loads(source_caixa.text)
+#data_bradesco = json.loads(source_bradesco.text)
+data_itau = json.loads(source_itau.text)
+data_nubank = json.loads(source_nubank.text)
 
-response_bradesco = requests.get(api_bradesco["URLDados"])
-
-data_bradesco = json.loads(response_bradesco.text)
-type(data_bradesco)
-print(data_bradesco)
+#print(data_bradesco)
+df_bradesco = pd.read_json(source_bradesco)
+print(df_bradesco.dtypes)
+hist_bradesco = df_bradesco.iloc[:,[2]]
+print(hist_bradesco)
